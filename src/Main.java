37c816bc9de875df690ea839c410fc3fcb7abcf3
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-08-30 10:08:48 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Min and Max Number Challenge!");
+
+        double min = 0;
+        double max = 0;
+        while(true){
+            System.out.println("Enter a number or any char to quit");
+            try{
+                String userInput = scanner.nextLine();
+                double tempNum = Double.parseDouble(userInput);
+                if (min == 0){
+                    min = tempNum;
+                }
+                if(tempNum > max){
+                   max = tempNum;
+                } else if (tempNum < min){
+                    min = tempNum;
+                }
+
+            }catch(NumberFormatException nfe){
+                break;
+            }
+        }
+        System.out.println("min: " + min + " max: " + max);
     }
 }
